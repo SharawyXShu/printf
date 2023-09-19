41337e7f6b4d0b688570b5c1cef31594f57302f1
alaa2003515 <lolomuhammd21@gmail.com>
2023-09-19 13:13:26 -0700

    printf

---
+++ _printf_int.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * _putchar - a function 
+ *Return:
+ */
+int _putchar(char so)
+{
+return(write(1, &so, 1));
+}
+
+void _case_i_d_int (Famous)
+{
+  int raqm = va_arg(Famous, int);
+  int orko = 1;
+  if (raqm < 0)
+    {
+      _putchar('-')
+	finally_yes++;
+      raqm *= -1;
+    }
+  while ((raqm / orko) > 9)
+    raqm *= 10;
+  while (raqm != 0)
+    {
+      _putchar()
+       raqm /= 10;
+    }
+
+int _printf(const char *format, ...);
