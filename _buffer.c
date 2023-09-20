e137169f138d6a8e5d15f9deec797a056e73b417
alaa2003515 <lolomuhammd21@gmail.com>
2023-09-20 15:24:10 -0700

    printf

--- _buffer.c
+++ _buffer.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
-void case_1(va_list lonly, char *buffer, int boxs)
+int case_1(va_list lonly, char *buffer, int boxs)
 {
 buffer[boxs] = (char)va_arg(lonly, int);
 return(boxs + 1);
 }
 
-void _buffer(char * box
+void _buffer(char *buffer , int boxs)
 {
 write(1, &buffer, boxs);
 }
@@ -17,37 +17,61 @@ char *star = va_arg(lonly, char *);
  if(star == NULL)
    star = "(null)";
  while ((star[frist_n] != '\0') && (boxs < (BUFFER_SIZE - 1)))
-   {
-     buffer[boxs] = star[frist_n];
-     boxs++;
-     fris_n++;
-   }
- return(boxs);
-}
-int _printf(const char *mat, ...)
 {
-  va_list lonly;
-  char buffer[BUFFER_SIZE];
-  int FINALLY_YES =0,boxs = 0;
-  if (mat == NULL)
-    return (0);
-  va_start(lonly, mat);
-  while (*mat)
-    {
-      if (*mat == '%')
-	{
-	  mat++;
-	  switch (*mat)
-	    {
-	      case 'c':
-	      boxs = case_1( lonly,buffer, boxs);
-	      break;
-	    case 's':
-	      boxs = case_2( lonly,buffer, boxs);
-	      break;
-	    case '%':
-	      buffer[boxs] = '%';
-	      boxs++;
-	      break;
-	    default:
-	      buffer[boxs] = '%';
+buffer[boxs] = star[frist_n];
+boxs++;
+frist_n++;
+}
+return(boxs);
+}
+int _printf_buffer(const char *mat, ...)
+{
+va_list lonly;
+char buffer[BUFFER_SIZE];
+int FINALLY_YES =0,boxs = 0;
+if (mat == NULL)
+return (0);
+va_start(lonly, mat);
+while (*mat)
+{
+if (*mat == '%')
+{
+mat++;
+switch (*mat)
+{
+case 'c':
+boxs = case_1( lonly,buffer, boxs);
+break;
+case 's':
+boxs = case_2( lonly,buffer, boxs);
+break;
+case '%':
+buffer[boxs] = '%';
+boxs++;
+break;
+default:
+buffer[boxs] = '%';
+boxs++;
+buffer[boxs] = *mat;
+boxs++;
+break;
+}
+}
+else
+{
+buffer[boxs] = *mat;
+boxs++;
+}
+mat++;
+if (boxs >= BUFFER_SIZE - 1)
+{
+_buffer(buffer ,boxs);
+FINALLY_YES += boxs;
+boxs = 0;
+}
+}
+_buffer(buffer , boxs);
+FINALLY_YES += boxs;
+va_end(lonly);
+return(FINALLY_YES);
+}
--- main.h
+++ main.h
@@ -16,6 +16,9 @@ int _printf(const char *format, ...);
 int _case_c(va_list lonly);
 int _case_s(va_list lonly);
 int _print_string(char *star);
+int case_1(va_list lonly, char *buffer, int boxs);
+void _buffer(char *buffer , int boxs);
 
-
+int case_2(va_list lonly, char *buffer, int boxs);
+int _printf_buffer(const char *mat, ...);
 #endif
