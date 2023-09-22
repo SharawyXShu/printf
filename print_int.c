57284147f138d0fe1162924886e235f77b0e9c62
alaa2003515 <lolomuhammd21@gmail.com>
2023-09-22 14:26:22 -0700

    printf

--- print_int.c
+++ print_int.c
@@ -8,7 +8,12 @@ int green(int so)
 {
 int totally = 0;
 unsigned int sox;
-if (so < 0)
+if (so == INT_MIN)
+{
+totally += _putchar('-');
+sox = INT_MAX + 1U;
+}
+else if (so < 0)
 {
 totally += _putchar('-');
 sox = -so;
